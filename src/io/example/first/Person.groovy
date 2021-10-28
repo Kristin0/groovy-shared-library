d2c2139f4aa9f5d7561cec9ce82bc51af64fc056
Kristin0 <kris123kris99@mail.ru>
2021-10-29 00:38:16 +0400

    groovy folder create

---
+++ src/io/example/first/Person.groovy
@@ -0,0 +1,14 @@
+package io.example
+ 
+class Person {
+
+    Person(def name, def age){
+        this.name = name
+        this.age = age
+    }
+
+    def personAbout(def name, def age){
+        return "Name: ${name}, age: ${age}" 
+    }
+}
+
